722757eed58429244caf487accbf2180a1fa7f23
Yukino Song <syqlds@126.com>
2017-04-08 15:51:52 +0800

    Upgrade version

--- src/escape-parser.js
+++ src/escape-parser.js
@@ -8,16 +8,36 @@ const n = new RegExp(`\\${char}n`, 'g')
 const v = new RegExp(`\\${char}v`, 'g')
 const f = new RegExp(`\\${char}f`, 'g')
 const r = new RegExp(`\\${char}r`, 'g')
+const oct = new RegExp(`\\${char}[0,7]{1,3}`, 'g')
+const ucp = new RegExp(`\\${char}u\\{.*\\}`, 'g')
 const uni = new RegExp(`\\${char}u.{0,4}`, 'g')
 const hex = new RegExp(`\\${char}x.{0,2}`, 'g')
 const esc = new RegExp(`\\${char}`, 'g')
+
+// Escape octonary sequence
+const O2C = (val) => {
+	val = val.substring(1)
+	val = parseInt(val, 8)
+	return String.fromCharCode(val)
+}
+
+// Escape unicode code point sequence
+const UC2C = (val) => {
+	val = val.substr(3, val.length - 4)
+	if (!val) throw new SyntaxError('Invalid Unicode escape sequence')
+	try {
+		return String.fromCodePoint(val)
+	} catch (err) {
+		throw new SyntaxError('Undefined Unicode code-point')
+	}
+}
 
 // Escape unicode sequence
 const U2C = (val) => {
 	val = val.substring(2)
 	val = parseInt(val, 16)
 	if (!val) throw new SyntaxError('Invalid Unicode escape sequence')
-	return String.fromCodePoint(val)
+	return String.fromCharCode(val)
 }
 
 // Escape hexadecimal sequence
@@ -25,7 +45,7 @@ const X2C = (val) => {
 	val = `00${val.substring(2)}`
 	val = parseInt(val, 16)
 	if (!val) throw new SyntaxError('Invalid hexadecimal escape sequence')
-	return String.fromCodePoint(val)
+	return String.fromCharCode(val)
 }
 
 const ESCAPE = (string) => {
@@ -42,6 +62,8 @@ const ESCAPE = (string) => {
 			.replace(v, '\v')
 			.replace(f, '\f')
 			.replace(r, '\r')
+			.replace(oct, O2C)
+			.replace(ucp, UC2C)
 			.replace(uni, U2C)
 			.replace(hex, X2C)
 			// Remove all useless escape characters
