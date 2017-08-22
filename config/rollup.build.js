2957b6e6fc7b7117609f6fae463a01d81758626c
Yukino Song <syqlds@126.com>
2017-08-22 18:13:56 +0800

    Upgrade version

--- config/rollup.build.js
+++ config/rollup.build.js
@@ -1,11 +1,7 @@
-// Set up environment
-import 'shelljs/global'
-if (env.BUILD_TARGET !== 'DEMO') env.NODE_ENV = 'production'
-
 // Import base config
 import base from './rollup.base'
 
 base.output.file = base.proDest
-base.output.sourcemap = env.BUILD_ENV === 'DEMO' || env.BUILD_ENV === 'CI' ? base.output.sourcemap : false
+base.output.sourcemap = process.env.BUILD_ENV === 'DEMO' || process.env.BUILD_ENV === 'CI' ? base.output.sourcemap : false
 
 export default base
